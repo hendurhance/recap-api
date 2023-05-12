7b63a71163fe734f3c49a14d04f7a38ba448d77e
Endurance, the Martian <afriviral@gmail.com>
2023-05-12 16:28:30 +0100

    fixes/ ommited movies/controller

---
+++ app/movies/controller.py
@@ -0,0 +1,24 @@
+import app.movies.service as service
+from app.core.redis_cache import cache_response, get_cached_response
+
+'''
+    @brief: This file is used from the movies routes to interact with the services
+'''
+
+def get_upcoming_movies(skip: int = 0, limit: int = 10, country: str = "US", type: str = "MOVIE"):
+    redis_key = f"upcoming_movies:{skip}:{limit}:{country}:{type}"
+    cached_data = get_cached_response(redis_key)
+    if cached_data is None:
+        response = service.scrape_upcoming_movies(skip, limit, country, type)
+        cache_response(redis_key, response, 60 * 60 * 24)
+        return response
+    return cached_data
+
+def get_top_rated_movies(skip: int = 0, limit: int = 10, sort_by : str = "RANKING", sort_type: str = "asc", page: int = 1):
+    redis_key = f"top_rated_movies:{skip}:{limit}:{sort_by}:{sort_type}:{page}"
+    cached_data = get_cached_response(redis_key)
+    if cached_data is None:
+        response = service.scrape_top_rated_movies(skip, limit, sort_by, sort_type, page)
+        cache_response(redis_key, response, 60 * 60 * 24)
+        return response
+    return cached_data
